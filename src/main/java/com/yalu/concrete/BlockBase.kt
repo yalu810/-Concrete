bcab3fec110b060798b843946a727f7a7175b8bf
yalu810 <yalughost810@gmail.com>
2021-01-30 19:12:20 +0800

    改正名稱

--- src/main/java/com/yalu/concrete/BlockBase.kt
+++ src/main/java/com/yalu/concrete/BlockBase.kt
@@ -4,6 +4,6 @@ import cpw.mods.fml.common.registry.GameRegistry
 import net.minecraft.block.Block
 import net.minecraft.block.material.Material
 
-class BlockTable(material: Material?) : Block(material) {
+class BlockBase(material: Material?) : Block(material) {
 
 }
