09943537e2029985475bdf4ca910104178f06eee
yalu810 <yalughost810@gmail.com>
2021-01-30 19:16:43 +0800

    改正名稱

--- src/main/java/com/yalu/concrete/Main.java
+++ src/main/java/com/yalu/concrete/Main.java
@@ -15,22 +15,22 @@ public class Main {
     }
 
     void registerBlocks() {
-        final Block blackConcrete = new BlockTable(Material.cloth).setBlockName("black_concrete").setBlockTextureName("concrete:black_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block blueConcrete = new BlockTable(Material.cloth).setBlockName("blue_concrete").setBlockTextureName("concrete:blue_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block brownConcrete = new BlockTable(Material.cloth).setBlockName("brown_concrete").setBlockTextureName("concrete:brown_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block cyanConcrete = new BlockTable(Material.cloth).setBlockName("cyan_concrete").setBlockTextureName("concrete:cyan_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block grayConcrete = new BlockTable(Material.cloth).setBlockName("gray_concrete").setBlockTextureName("concrete:gray_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block greenConcrete = new BlockTable(Material.cloth).setBlockName("green_concrete").setBlockTextureName("concrete:green_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block lightBlueConcrete = new BlockTable(Material.cloth).setBlockName("light_blue_concrete").setBlockTextureName("concrete:light_blue_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block lightGrayConcrete = new BlockTable(Material.cloth).setBlockName("light_gray_concrete").setBlockTextureName("concrete:light_gray_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block limeConcrete = new BlockTable(Material.cloth).setBlockName("lime_concrete").setBlockTextureName("concrete:lime_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block magentaConcrete = new BlockTable(Material.cloth).setBlockName("magenta_concrete").setBlockTextureName("concrete:magenta_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block orangeConcrete = new BlockTable(Material.cloth).setBlockName("orange_concrete").setBlockTextureName("concrete:orange_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block pinkConcrete = new BlockTable(Material.cloth).setBlockName("pink_concrete").setBlockTextureName("concrete:pink_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block purpleConcrete = new BlockTable(Material.cloth).setBlockName("purple_concrete").setBlockTextureName("concrete:purple_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block redConcrete = new BlockTable(Material.cloth).setBlockName("red_concrete").setBlockTextureName("concrete:red_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block whiteConcrete = new BlockTable(Material.cloth).setBlockName("white_concrete").setBlockTextureName("concrete:white_concrete").setCreativeTab(CreativeTabs.tabBlock);
-        final Block yellowConcrete = new BlockTable(Material.cloth).setBlockName("yellow_concrete").setBlockTextureName("concrete:yellow_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block blackConcrete = new BlockBase(Material.cloth).setBlockName("black_concrete").setBlockTextureName("concrete:black_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block blueConcrete = new BlockBase(Material.cloth).setBlockName("blue_concrete").setBlockTextureName("concrete:blue_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block brownConcrete = new BlockBase(Material.cloth).setBlockName("brown_concrete").setBlockTextureName("concrete:brown_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block cyanConcrete = new BlockBase(Material.cloth).setBlockName("cyan_concrete").setBlockTextureName("concrete:cyan_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block grayConcrete = new BlockBase(Material.cloth).setBlockName("gray_concrete").setBlockTextureName("concrete:gray_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block greenConcrete = new BlockBase(Material.cloth).setBlockName("green_concrete").setBlockTextureName("concrete:green_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block lightBlueConcrete = new BlockBase(Material.cloth).setBlockName("light_blue_concrete").setBlockTextureName("concrete:light_blue_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block lightGrayConcrete = new BlockBase(Material.cloth).setBlockName("light_gray_concrete").setBlockTextureName("concrete:light_gray_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block limeConcrete = new BlockBase(Material.cloth).setBlockName("lime_concrete").setBlockTextureName("concrete:lime_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block magentaConcrete = new BlockBase(Material.cloth).setBlockName("magenta_concrete").setBlockTextureName("concrete:magenta_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block orangeConcrete = new BlockBase(Material.cloth).setBlockName("orange_concrete").setBlockTextureName("concrete:orange_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block pinkConcrete = new BlockBase(Material.cloth).setBlockName("pink_concrete").setBlockTextureName("concrete:pink_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block purpleConcrete = new BlockBase(Material.cloth).setBlockName("purple_concrete").setBlockTextureName("concrete:purple_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block redConcrete = new BlockBase(Material.cloth).setBlockName("red_concrete").setBlockTextureName("concrete:red_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block whiteConcrete = new BlockBase(Material.cloth).setBlockName("white_concrete").setBlockTextureName("concrete:white_concrete").setCreativeTab(CreativeTabs.tabBlock);
+        final Block yellowConcrete = new BlockBase(Material.cloth).setBlockName("yellow_concrete").setBlockTextureName("concrete:yellow_concrete").setCreativeTab(CreativeTabs.tabBlock);
         
         GameRegistry.registerBlock(blackConcrete, blackConcrete.getUnlocalizedName().substring(5));
         GameRegistry.registerBlock(blueConcrete, blueConcrete.getUnlocalizedName().substring(5));
